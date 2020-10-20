b4a99f655dbc0b89578b23a892a353243565f4c7
Ruben <rubentrevino95@gmail.com>
2020-10-20 18:57:11 -0500

    movies now play off of franchises index

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 var movies = [
     {
         series: 'Halloween',
-        urlList: []
+        urlList: [
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+(1978).mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+2.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+3.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+4.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+5+The+Revenge+of+Michael+Myers.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+6+The+Curse+of+Michael+Myers.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+7+H20.mp4',
+            'https://h-flix.s3.us-east-2.amazonaws.com/Halloween+Collection/Halloween+8++Resurrection.mp4'
+        ]
     },
     {
         series: 'Nightmare on Elm Street',
@@ -21,7 +30,12 @@ var movies = [
     },
     {
         series: 'Friday the 13th',
-        urlList: []
+        urlList: [
+            "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+1.mp4",
+            "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+2.mp4",
+            "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+3.mp4",
+            "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+4+The+Final+Chapter.mp4"
+        ]
     },
     {
         series: 'Wildcard',
@@ -30,32 +44,42 @@ var movies = [
 ];
 
 
-var video_list = [
-    "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+1.mp4",
-    "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+2.mp4",
-    "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+3.mp4",
-    "https://h-flix.s3.us-east-2.amazonaws.com/friday+the+13th+collection/Friday+The+13th+Part+4+The+Final+Chapter.mp4"
-];
+let halloween = movies[0].urlList;
+let elmStreet = movies[1].urlList;
+let scream = movies[2].urlList;
+let childsPlay = movies[3].urlList;
+let texasChainsaw = movies[4].urlList;
+let friday = movies[5].urlList;
+let wildcard = movies[6].urlList;
+console.log(halloween);
+
 var video_index = 0;
 var video_player = null;
 
-console.log(video_index);
+// storing url arrays into new array
+let franchises = new Array(halloween, elmStreet, scream, childsPlay, texasChainsaw, friday, wildcard);
 
+
+
+//src switch
 function onload() {
-    console.log("body loaded");
     video_player = document.getElementById("video");
-    video_player.setAttribute("src", video_list[video_index]);
+    // set a condition here
+
+    // sets src
+    video_player.setAttribute("src", franchises[5][video_index]);
     video_player.play();
 }
 
+//incrementor
 function onVideoEnded() {
     console.log("video ended");
-    if(video_index < video_list.length - 1) {
+    if(video_index < franchises[5].length - 1) {
         video_index++;
     }
     else {
         video_index = 0;
     }
-    video_player.setAttribute("src", video_list[video_index]);
+    video_player.setAttribute("src", franchises[5][video_index]);
     video_player.play();
 }
